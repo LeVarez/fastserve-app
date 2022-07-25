54b21cb6a83e46da1819573cdeb804b168de6990
LeVarez <aleixac1997@gmail.com>
2022-07-25 16:56:42 +0200

    as

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,12 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-      // default options are shown
-      out: 'build',
-      precompress: false,
-      envPrefix: ''
-    }),
+		adapter: adapter(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
